592a3007b1c2ad1523a916d16bcba259bf34a143
Jaime <jaimet7936824@gmail.com>
2026-05-17 18:15:17 -0500

    Optimize docker entrypoint for Render and port binding

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-vendor/bin/server run 0.0.0.0:8080 --adapter=App\\AppKernelAdapter --workers=-1
+PORT_TO_USE=${PORT:-8080}
+vendor/bin/server run 0.0.0.0:$PORT_TO_USE --adapter=App\\AppKernelAdapter --workers=-1
+
 
